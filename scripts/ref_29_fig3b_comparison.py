ec773657eddd3887270d1b4af5da569b763b10cf
Ohad Lev <ohadlev77@gmail.com>
2025-07-02 15:31:56 +0300

    Add detailed comparisons to ref [29] (PhysRevA.109.052441) w.r.t 'Figure 3b' and 'TABLE I' in ref [29] - data generation scripts and plots generation notebooks (under 'scripts' directory), experimental data (under 'EXP_DATA' directory)

---
+++ scripts/ref_29_fig3b_comparison.py
@@ -0,0 +1,46 @@
+import networkx as nx
+from qiskit_aer import StatevectorSimulator
+
+from qemc.benchmarking.executer import QEMCExecuter
+
+
+# Configuration for the experiment
+num_algorithm_repeats = 50
+maxiter = 750
+num_layers = [3, 4, 5, 6, 7]
+rhobegs = [0.8, 0.9, 1, 1.1, 1.2]
+
+# Graphs generation parameters
+num_nodes = 32
+probs_vec = [0.66]
+seed = 0
+
+
+graphs = []
+for p in probs_vec:
+    graph: nx.Graph = nx.fast_gnp_random_graph(n=num_nodes, p=p, seed=seed)
+    graph_density = nx.density(graph)
+    graph.name = f"random_graph__p_{p:.3f}__density_{graph_density:.2f}__seed_{seed}"
+
+    print()
+    print(f"Graph: {graph.name}")
+    print(f"{graph.number_of_edges()=}")
+    print(f"{nx.is_connected(graph)=}")
+    print(f"{graph_density=}")
+
+    graphs.append(graph)
+
+
+executer = QEMCExecuter(experiment_name="ref_29_fig3b_comparison")
+executer.define_graphs(graphs)
+executer.define_qemc_parameters(
+    shots=[None],
+    num_layers=num_layers,
+    num_blue_nodes=[None],
+)
+executer.define_optimization_process(
+    optimization_method="COBYLA",
+    optimization_options=[{"maxiter": maxiter, "rhobeg": rhobeg} for rhobeg in rhobegs],
+)
+executer.define_backends([StatevectorSimulator()])
+executer.execute_export(num_samples=num_algorithm_repeats, export_path="EXP_DATA")
