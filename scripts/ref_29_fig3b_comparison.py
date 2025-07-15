73bbca63456de3a5deba6a89fdf532de0474c55b
Ohad Lev <ohadlev77@gmail.com>
2025-07-15 10:08:32 +0300

    Add comparison data to ref [29] (PhysRevA.109.052441) Figure 3b - with multiple B values (|V|/2, |V|/2 - 2, |V|/2 -4)

--- scripts/ref_29_fig3b_comparison.py
+++ scripts/ref_29_fig3b_comparison.py
@@ -4,16 +4,19 @@ from qiskit_aer import StatevectorSimulator
 from qemc.benchmarking.executer import QEMCExecuter
 
 
+# Graphs generation parameters
+num_nodes = 32
+probs_vec = [0.66]
+seed = 0
+
 # Configuration for the experiment
 num_algorithm_repeats = 50
 maxiter = 750
 num_layers = [3, 4, 5, 6, 7]
 rhobegs = [0.8, 0.9, 1, 1.1, 1.2]
 
-# Graphs generation parameters
-num_nodes = 32
-probs_vec = [0.66]
-seed = 0
+B = num_nodes / 2
+num_blue_nodes = [B, B - 2, B - 4]
 
 
 graphs = []
@@ -31,12 +34,12 @@ for p in probs_vec:
     graphs.append(graph)
 
 
-executer = QEMCExecuter(experiment_name="ref_29_fig3b_comparison")
+executer = QEMCExecuter(experiment_name="ref_29_fig3b_comparison_multiple_B_values")
 executer.define_graphs(graphs)
 executer.define_qemc_parameters(
     shots=[None],
     num_layers=num_layers,
-    num_blue_nodes=[None],
+    num_blue_nodes=num_blue_nodes,
 )
 executer.define_optimization_process(
     optimization_method="COBYLA",
